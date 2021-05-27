e3194fc2e415d5da601acf15547fda9281d29ae1
Jairo Junior <jairo.ajunior@mercadolivre.com>
2021-05-27 11:56:38 -0300

    Adicionando as melhorias sugeridas pelo amadeos

--- src/main/java/br/com/meli/exercises/controller/AgeController.java
+++ src/main/java/br/com/meli/exercises/controller/AgeController.java
@@ -1,14 +1,17 @@
 package br.com.meli.exercises.controller;
 
-import br.com.meli.exercises.service.impl.AgeServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.meli.exercises.service.AgeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AgeController {
-    public AgeServiceImpl ageService = new AgeServiceImpl();
+    private final AgeService ageService;
+
+    public AgeController(AgeService ageService) {
+        this.ageService = ageService;
+    }
 
     @GetMapping("/{day}/{month}/{year}")
     public int getAgeByBornDate(@PathVariable int day, @PathVariable int month, @PathVariable int year) {
